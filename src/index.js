77cd54c466ec3f06cf8a9ab50400d683027b0bfe
EC2 Default User <ec2-user@ip-172-31-80-182.ec2.internal>
2020-07-26 10:18:02 +0000

    first reall commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import Amplify, { Analytics } from 'aws-amplify'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import AppProvider from './context/AppProvider'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+import App from './App';
+import Product from './pages/Product'
+import Checkout from './pages/Checkout'
+import PlacedOrder from './pages/PlacedOrder'
+
+import awsconfig from './aws-exports'
+Amplify.configure(awsconfig)
+
+Analytics.record('Store_Load');
+            
+Analytics.autoTrack('pageView', {
+enable: true,
+type: 'SPA'
+})
+
+const routing = (
+<AppProvider>
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/product/:id" component={Product} />
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/ordercomplete" component={PlacedOrder}/>
+            <Route component={App} />
+        </Switch>
+    </Router>
+</AppProvider>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
 serviceWorker.unregister();
+
